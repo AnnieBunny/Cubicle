6cf2de30ff1e5b4cb5f1e385fa45c55f39effdc7
Annie <75939205+AnnieBunny@users.noreply.github.com>
2021-01-29 22:01:48 +0200

    add json db

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,23 +1,28 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const productService = require('../services/productService');
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home', {title : 'Browse'})
+    res.render('home', { title: 'Browse' })
 });
 
 router.get('/create', (req, res) => {
-    res.render('create', {title : 'Create'})
+    res.render('create', { title: 'Create' })
 });
 
 router.post('/create', (req, res) => {
-  
+
+    productService.create( req.body)
+   
+   
+        
 
     res.redirect('/products')
 });
 
-router.get('/details/:productId', (req,res) => {
-    res.render('details', {title : 'Details'})
+router.get('/details/:productId', (req, res) => {
+    res.render('details', { title: 'Details' })
 });
 
 module.exports = router;
